01dd1383d22fc8f11b4cda03507881a5a043e93b
don <bandy@gmail.com>
2021-07-14 19:12:24 -0700

    added state and how many times favorited

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
+import './HornedBeast.css'
+
 class HornedBeast extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      timesFavorited: 0,
+    }
+  }
+
   render() {
+
     return (
-      <>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description}/>
-        <p>{this.props.description}</p> 
-      </>
+      <div>
+        <>
+          <h2>{this.props.title}</h2>
+          <img src={this.props.imageUrl} alt={this.props.description}/>
+          <p>{this.props.description}</p> 
+          <p>Favorited {this.state.timesFavorited} times</p>
+        </>
+      </div>
       
     )
   }
